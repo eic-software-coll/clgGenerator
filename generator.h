4bfe30573e31208066da1d11fbe9ce566d44f441
mauri <maureeungaro@gmail.com>
2015-02-05 12:05:29 -0500

    first few implementations

---
+++ generator.h
@@ -0,0 +1,18 @@
+#ifndef CLGGENERATOR
+#define CLGGENERATOR 1
+
+class clgGenerator
+{
+
+	private:
+		double timeWindow;   // TimeWindow of the 
+
+
+	
+
+
+
+
+}
+
+#endif
---
+++ particle.h
@@ -0,0 +1,18 @@
+#ifndef CLGPARTICLE
+#define CLGPARTICLE 1
+
+class clgParticle
+{
+
+	private:
+		int pid;   // MonteCarlo PID scheme
+
+
+	
+
+
+
+
+}
+
+#endif
